cd71524b2cce783e0a32546073225844d6fc0e75
Bernardo Subercaseaux <bernardosubercaseaux@gmail.com>
2017-10-30 15:14:53 -0400

    first solution in C++

--- aeropuerto/solutions/correct/bernardo.cpp
+++ aeropuerto/solutions/correct/bernardo.cpp
@@ -10,10 +10,35 @@ struct TE {
 };
 
 // Temporary dijkstra method
-void temporary_dijkstra(vector<int> &distances, vector<vector<temporary_edge>> &tes, int start) {
-	
-	while(!q.empty()){
+void temporary_dijkstra(vector<vector<pair<int,int>> &graph, vector<int> &distances, 
+					vector<vector<temporary_edge>> &tes, int start) {
+	distnces[start] = 0; 
+        priority_queue<pair<int,int>> pq;
+	pq.push({0,start});
+	while(!pq.empty()){
+		pair<int, int> p = pq.top(); p.pop();
+		u = p.second; priority = p.first;
+		if(priority != distance[u]) {
+			continue;	
+		}
+		for(pair<int, int>  par : graph[u]){
+			int u = pair.first; int w = pair.second;
+			if(distances[u] + w < distances[v]){
+				distances[v] = distances[u] + w;
+				pq.push({distances[v], v});
+			}	
+		}
 
+// modification of usual Dijkstra's algorithm to process the temporary edges
+		for(TE te : tes[u]) {
+			if(te.starting_time >= distances[u]){
+				int v = te.destiny;
+				if(distances[u] + te.length < distances[v]) {
+					distances[v] = distances[u] + te.length;
+					pq.push({distances[v], v});
+				}
+			}
+		}
 	} 	
 }
 
@@ -41,7 +66,7 @@ int main() {
 
 	vector<int> distances(N, 1e9); // initialized to infinity
 	// fill the distance array	
-	temporary_dijkstra(distances, tes, S);
+	temporary_dijkstra(graph, distances, tes, S);
 	// answer
 	cout << distances[T] << endl;
 	return 0;
