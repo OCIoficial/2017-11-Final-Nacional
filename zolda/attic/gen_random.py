7b36eb5d9f528214bd66eb98fd21df3183d6a0f6
jpaulsen <juanpablo.paulsen@gmail.com>
2017-10-31 19:28:05 -0300

    add test plan to zolda

---
+++ zolda/attic/gen_random.py
@@ -0,0 +1,31 @@
+import random
+import sys
+
+random.seed(hash(str(sys.argv)))
+
+n = int(sys.argv[2])
+k = int(sys.argv[3])
+minX = int(sys.argv[4])
+maxX = int(sys.argv[5])
+minY = int(sys.argv[6])
+maxY = int(sys.argv[7])
+
+print(str(n) + " " + str(k))
+
+points = set()
+for _ in range(n):
+    while True:
+        x = random.randint(minX, maxX)
+        y = random.randint(minY, maxY)
+        flipX = random.randint(0, 1)
+        flipY = random.randint(0, 1)
+        if flipX == 1:
+            x *= -1
+        if flipY == 1:
+            y *= -1
+        if (x, y) not in points:
+            break
+    points.add((x, y))
+
+for x, y in points:
+    print(x, y)
