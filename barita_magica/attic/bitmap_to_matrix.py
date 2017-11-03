3699c10e2f531b4b9e35ba9eb1089f537a62cfc4
Nico Lehmann <nlehmann@dcc.uchile.cl>
2017-11-02 21:35:44 -0700

    Script para transformar imagenes a matriz

---
+++ barita_magica/attic/bitmap_to_matrix.py
@@ -0,0 +1,24 @@
+import re
+import sys
+import subprocess
+
+outcome = subprocess.run(['convert', sys.argv[1], 'txt:-'], stdout=subprocess.PIPE, universal_newlines=True)
+
+regex = re.compile('(\d+),(\d+): \(\d+,\d+,\d+,(\d+)\) ')
+
+matrix = [[0 for x in range(50)] for y in range(50)]
+for line in outcome.stdout.split('\n'):
+    m = regex.match(line)
+    if m:
+        x = int(m.group(1))
+        y = int(m.group(2))
+        opacity = int(m.group(3))
+        if opacity > 0:
+            matrix[y][x] = 1
+
+for row in matrix:
+    for i, col in enumerate(row):
+        if i > 0:
+            print(' ', end='')
+        print(col, end='')
+    print()
