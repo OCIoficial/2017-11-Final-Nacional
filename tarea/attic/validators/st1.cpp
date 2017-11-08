8fbb132eb1963d25d980a532c44dec3b9b92a818
Nico Lehmann <nlehmann@dcc.uchile.cl>
2017-11-07 22:03:55 -0800

    Validador para tarea + fix en testplan

---
+++ tarea/attic/validators/st1.cpp
@@ -0,0 +1,19 @@
+/*
+ * -----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <nlehmann@dcc.uchile.cl> wrote this file. As long as you retain this notice
+ * you can do whatever you want with this stuff. If we meet some day, and you
+ * think this stuff is worth it, you can buy me a beer in return Nicolás Lehmann
+ * -----------------------------------------------------------------------------
+ */
+#include "testlib.h"
+using namespace std;
+
+int main() {
+  registerValidation();
+  int N = inf.readInt(1, 1000000, "N");
+  inf.readEoln();
+  inf.readEof();
+
+  return 0;
+}
